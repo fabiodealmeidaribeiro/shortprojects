72f20cff027ab3777be1df32dcb03cd4cdc57958
fabiodealmeidaribeiro <far040615@gmail.com>
2023-05-22 03:23:53 -0300

    2023/05/22-03:23

--- components/light-box/v1/script.js
+++ components/light-box/v1/script.js
@@ -1,14 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // ===> DOM elements <=== //
-
-    const $imagesContainer = document.getElementById('images-container');
+    const $imagesContainer = document.getElementById('container');
     const $lightbox = document.getElementById('lightbox');
-
-    // ===> Event listeners and triggers <=== //
-
-    // Show lightbox 
     $imagesContainer.addEventListener('click', e => {
-        const imageWrapper = e.target.closest('.image-wrapper');
+        const imageWrapper = e.target.closest('.wrapper');
         if (imageWrapper) {
             const image = imageWrapper.querySelector('img');
             if (image) {
@@ -17,16 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         }
     });
-
-    // Hide Lightbox
     $lightbox.addEventListener('click', (e) => {
         if (!e.target.hasAttribute('src')) {
             $lightbox.classList.remove('show');
         }
     });
-
-    // Loading...
-    setTimeout(() =>
-        $imagesContainer.classList.remove('loading')
-        , 1500);
+    setTimeout(() => $imagesContainer.classList.remove('loading') , 1500);
 });
